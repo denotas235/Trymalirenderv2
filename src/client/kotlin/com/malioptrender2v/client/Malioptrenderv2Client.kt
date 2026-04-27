da0608f51e5026ded438b911406e212b0c7f4f6d
denotaravys <denotaravys@gmail.com>
2026-04-27 19:36:41 +0200

    feat: adicionar deteção de hardware Mali e fix de javadoc

--- src/client/kotlin/com/malioptrender2v/client/Malioptrenderv2Client.kt
+++ src/client/kotlin/com/malioptrender2v/client/Malioptrenderv2Client.kt
@@ -1,9 +1,22 @@
 package com.malioptrender2v.client
 
 import net.fabricmc.api.ClientModInitializer
+import org.lwjgl.opengl.GL11
+import org.slf4j.LoggerFactory
 
 object Malioptrenderv2Client : ClientModInitializer {
-	override fun onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
+    private val LOGGER = LoggerFactory.getLogger("MaliOpt")
+
+    override fun onInitializeClient() {
+        val renderer = GL11.glGetString(GL11.GL_RENDERER) ?: "Desconhecido"
+        val vendor = GL11.glGetString(GL11.GL_VENDOR) ?: "Desconhecido"
+
+        LOGGER.info("Iniciando MaliOpt para 1.21.11")
+        
+        if (renderer.contains("Mali", ignoreCase = true) || vendor.contains("ARM", ignoreCase = true)) {
+            LOGGER.info("[MaliOpt] Hardware MALI detetado: $renderer")
+        } else {
+            LOGGER.info("[MaliOpt] Hardware não-Mali detetado: $renderer")
+        }
+    }
 }
