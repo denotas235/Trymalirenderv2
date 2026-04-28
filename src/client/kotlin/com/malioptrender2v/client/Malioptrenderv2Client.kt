11d60c2b369a36ec784311dd3f2c3cecb34e3f08
denotaravys <denotaravys@gmail.com>
2026-04-28 09:29:15 +0200

    Fix: Scanner movido para CLIENT_STARTED e Mixins ativadas

--- src/client/kotlin/com/malioptrender2v/client/Malioptrenderv2Client.kt
+++ src/client/kotlin/com/malioptrender2v/client/Malioptrenderv2Client.kt
@@ -1,22 +1,18 @@
 package com.malioptrender2v.client
 
 import net.fabricmc.api.ClientModInitializer
-import org.lwjgl.opengl.GL11
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents
 import org.slf4j.LoggerFactory
 
 object Malioptrenderv2Client : ClientModInitializer {
     private val LOGGER = LoggerFactory.getLogger("MaliOpt")
 
     override fun onInitializeClient() {
-        val renderer = GL11.glGetString(GL11.GL_RENDERER) ?: "Desconhecido"
-        val vendor = GL11.glGetString(GL11.GL_VENDOR) ?: "Desconhecido"
+        LOGGER.info("MaliOpt V2: Aguardando janela para ler hardware...")
 
-        LOGGER.info("Iniciando MaliOpt para 1.21.11")
-        
-        if (renderer.contains("Mali", ignoreCase = true) || vendor.contains("ARM", ignoreCase = true)) {
-            LOGGER.info("[MaliOpt] Hardware MALI detetado: $renderer")
-        } else {
-            LOGGER.info("[MaliOpt] Hardware não-Mali detetado: $renderer")
+        ClientLifecycleEvents.CLIENT_STARTED.register { client ->
+            LOGGER.info("[MaliOpt] Janela aberta. Iniciando scanner de GPU...")
+            MaliHardware.checkExtensions()
         }
     }
 }
