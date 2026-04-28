c67cfc24f340a0fe32a9186fd7a72814b267287a
denotaravys <denotaravys@gmail.com>
2026-04-28 12:28:20 +0200

    Fix: Adicionar repositórios de plugins e estabilizar versões

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
-    id("net.fabricmc.fabric-loom") version "1.7-SNAPSHOT"
+    id("net.fabricmc.fabric-loom") version "1.7.4"
     `maven-publish`
     id("org.jetbrains.kotlin.jvm") version "2.0.0"
 }
@@ -10,14 +10,14 @@ version = "1.0.0"
 group = "com.malioptrender2v"
 
 base {
-    archivesName.set("malioptrender2v")
+    archivesName.set("malioptrenderv2")
 }
 
 loom {
     splitEnvironmentSourceSets()
 
     mods {
-        register("malioptrender2v") {
+        register("malioptrenderv2") {
             sourceSet(sourceSets.main.get())
             sourceSet(sourceSets.getByName("client"))
         }
@@ -28,25 +28,12 @@ loom {
     }
 }
 
-fabricApi {
-    configureDataGeneration {
-        client = true
-    }
-}
-
 dependencies {
-    minecraft("com.mojang:minecraft:1.21.11")
+    minecraft("com.mojang:minecraft:1.21.1")
     mappings(loom.officialMojangMappings())
     modImplementation("net.fabricmc:fabric-loader:0.16.7")
     modImplementation("net.fabricmc.fabric-api:fabric-api:0.106.1+1.21.1")
     modImplementation("net.fabricmc:fabric-language-kotlin:1.12.1+kotlin.2.0.0")
-}
-
-tasks.processResources {
-    inputs.property("version", project.version)
-    filesMatching("fabric.mod.json") {
-        expand("version" to project.version)
-    }
 }
 
 tasks.withType<JavaCompile>().configureEach {
@@ -66,17 +53,7 @@ java {
 }
 
 tasks.jar {
-    val projectName = project.name
-    inputs.property("projectName", projectName)
     from("LICENSE") {
-        rename { "${it}_$projectName" }
-    }
-}
-
-publishing {
-    publications {
-        register<MavenPublication>("mavenJava") {
-            from(components["java"])
-        }
+        rename { "LICENSE_${project.name}" }
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,17 +1,22 @@
 pluginManagement {
-	repositories {
-		maven {
-			name = "Fabric"
-			url = uri("https://maven.fabricmc.net/")
-		}
-		mavenCentral()
-		gradlePluginPortal()
-	}
-
-	plugins {
-		id("net.fabricmc.fabric-loom-remap") version providers.gradleProperty("loom_version")
-	}
+    repositories {
+        maven {
+            name = "Fabric"
+            url = uri("https://maven.fabricmc.net/")
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
 }
 
-// Should match your modid
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+        maven {
+            name = "Fabric"
+            url = uri("https://maven.fabricmc.net/")
+        }
+    }
+}
+
 rootProject.name = "malioptrenderv2"
