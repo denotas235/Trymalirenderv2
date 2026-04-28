aeecc01498930ad32eae1468b781f134cef508c3
denotaravys <denotaravys@gmail.com>
2026-04-28 12:33:59 +0200

    Restauro: Voltando à configuração base funcional para corrigir o Loom

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,35 +1,27 @@
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-
 plugins {
-    id("net.fabricmc.fabric-loom") version "1.7.4"
-    `maven-publish`
+    id("net.fabricmc.fabric-loom") version "1.7-SNAPSHOT"
+    id("maven-publish")
     id("org.jetbrains.kotlin.jvm") version "2.0.0"
 }
 
 version = "1.0.0"
 group = "com.malioptrender2v"
 
-base {
-    archivesName.set("malioptrenderv2")
-}
-
 loom {
     splitEnvironmentSourceSets()
-
     mods {
         register("malioptrenderv2") {
             sourceSet(sourceSets.main.get())
             sourceSet(sourceSets.getByName("client"))
         }
     }
-
     mixin {
         defaultRefmapName.set("malioptrenderv2.refmap.json")
     }
 }
 
 dependencies {
-    minecraft("com.mojang:minecraft:1.21.1")
+    minecraft("com.mojang:minecraft:1.21.11")
     mappings(loom.officialMojangMappings())
     modImplementation("net.fabricmc:fabric-loader:0.16.7")
     modImplementation("net.fabricmc.fabric-api:fabric-api:0.106.1+1.21.1")
@@ -40,20 +32,8 @@ tasks.withType<JavaCompile>().configureEach {
     options.release.set(21)
 }
 
-kotlin {
-    compilerOptions {
-        jvmTarget.set(JvmTarget.JVM_21)
-    }
-}
-
 java {
     withSourcesJar()
     sourceCompatibility = JavaVersion.VERSION_21
     targetCompatibility = JavaVersion.VERSION_21
-}
-
-tasks.jar {
-    from("LICENSE") {
-        rename { "LICENSE_${project.name}" }
-    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,23 +1,15 @@
 pluginManagement {
     repositories {
-        maven {
-            name = "Fabric"
-            url = uri("https://maven.fabricmc.net/")
-        }
+        maven { url = uri("https://maven.fabricmc.net/") }
         mavenCentral()
         gradlePluginPortal()
     }
 }
-
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         mavenCentral()
-        maven {
-            name = "Fabric"
-            url = uri("https://maven.fabricmc.net/")
-        }
+        maven { url = uri("https://maven.fabricmc.net/") }
     }
 }
-
 rootProject.name = "malioptrenderv2"
